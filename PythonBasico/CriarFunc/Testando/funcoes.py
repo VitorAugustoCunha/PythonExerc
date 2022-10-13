6a1d48ae86a69f1dcb0a404262a7d999c9efc7cf
Vitor Augusto da CUnha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-10-13 10:13:34 -0300

    Update funcoes.py

--- PythonBasico/CriarFunc/Testando/funcoes.py
+++ PythonBasico/CriarFunc/Testando/funcoes.py
@@ -42,6 +42,115 @@ def mediasAlunos (n1,n2,nome):
     medias.append(media)
     return alunos, medias
 
+def loopUm():
+    n = 0
+    while n < 5:
+        print("Hello Pythonista")
+        n+=1
+        if n == 3:
+            break
+def loopDois():
+    n = 0
+    while n < 5:
+        if n == 2:
+            n+=1
+            continue
+        print("Hello Pythonista")
+        n+=1
+
+def loopTres():
+    z = 0
+    while z < 3:
+        if z == 0:
+            print("z é ",z)
+            z+=1
+        elif z == 1:
+            print("z é ",z)
+            z+=1
+        else:
+            print("z é ",z)
+            z+=1
+
+def listUm():
+    minhaLista = []
+    i = 0
+    while len(minhaLista) < 4:
+        minhaLista.append(i)
+        i+=1
+    print(minhaLista)
+
+def loopQuatro():
+    n = 10
+    while n <=100:
+        print(n, end = ";")
+        n+=10
+
+def listDois():
+    minhaTuple = (23, 45, 12, 10, 25)
+    i = 0
+
+    while i < len(minhaTuple):
+        print(minhaTuple[i])
+        i+=1
+
+def listTres():
+    minhaLista = [23, 45, 12, 10, 25]
+    i = 0
+    sum = 0
+
+    while i < len(minhaLista):
+        sum += minhaLista[i]
+        i+=1
+    print(sum)
+
+def listQuatro():
+    listaFrutas = ["Limão","Abacate","Jambo","Laranja"]
+
+    while listaFrutas:
+        print(listaFrutas.pop())
+    print(listaFrutas)
+
+def exerUm():
+    i = 0
+    palavra = "O êxito é ir de frustração a frustração sem perder a animação"
+
+    while i < len(palavra):
+        if palavra[i] == "e" or palavra[i] == "o":
+            i+=1
+            continue
+        print(palavra[i], end="")
+        i+=1
+
+def loopCinco():
+    n = int(input("Digite um número: "))
+
+    while n != 0:
+        n = int(input("Digite zero para sair: "))
+
+def loopSeis():
+    p = 5
+    soma = 0
+    count  = 0
+
+    while p > 0:
+        count+=1
+        f = int(input("Digite um número: "))
+        soma += f
+        p-=1
+
+    media = soma/count
+    print("Media do número recebidos ", media)
+
+def loopSete():
+    n = 1
+
+    while n <= 5:
+        quadradonum = n**2
+        print(f"O quadrado de {n} é {quadradonum}")
+        n+=1
+
+
+
 
 
 
