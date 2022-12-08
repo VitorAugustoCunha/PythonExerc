eeb42f1d93005b2fec70d680dcec1625d0bdda88
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-12-08 12:16:49 -0300

    Add files via upload

---
+++ POO_aula/Pessoa.py
@@ -0,0 +1,46 @@
+from datetime import  datetime
+class pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
+    def __init__(self, nome, idade, comendo = False, falando = False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+        self.ano_atual = self.ano_atual
+
+    def comer(self, alimento):
+        if self.comendo == True:
+            print(f'{self.nome} já está comendo {alimento}, não pode comer')
+            return
+        print(f'{self.nome} está comendo {alimento}')
+        self.comendo = True
+
+    def parar_Comer(self):
+        if self.comendo == True:
+            print(f'{self.nome} parou de comer')
+            self.comendo = False
+            return
+        print(f'{self.nome} não está comendo')
+
+    def falar(self, assunto):
+        if self.comendo == True:
+            print(f'{self.nome} não pode falar comendo')
+            return
+        if self.falando == True:
+            print(f'{self.nome} já está falando, não pode falar')
+            return
+        print(f'{self.nome} está falando {assunto}')
+        self.falando = True
+
+    def parar_Falar(self):
+        if self.comendo == True:
+            print(f'{self.nome} não pode para de falar, pois está comendo')
+            return
+        if self.falando == True:
+            print(f'{self.nome} parou de falar')
+            self.falando = False
+            return
+        print(f'{self.nome} não está falando')
+    def ano_nascimento(self):
+        return self.ano_atual - self.idade
---
+++ POO_aula/main.py
@@ -0,0 +1,7 @@
+from Pessoa import pessoa
+from datetime import datetime
+p1 = pessoa('vitor', 20)
+
+
+
+
