fda16e66e6a3c05b0e4729d26ce5bc0fa2a92c81
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-12-08 18:11:24 -0300

    Update escola.py

--- EstudosProvaJR/SQL - MySQL/Exercicio1/escola.py
+++ EstudosProvaJR/SQL - MySQL/Exercicio1/escola.py
@@ -1,53 +1,47 @@
-import mysql.connector
+from escola import Sala1
+from escola import Sala2
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="123456",
-  database="escola"
-)
-mycursor = mydb.cursor()
+continuar = True
 
-class Sala1:
-  def __init__(self, nome, nota1, nota2, nota3):
-    self.nome = nome
-    self.nota1 = nota1
-    self.nota2 = nota2
-    self.nota3 = nota3
+while continuar == True:
+    h = int(input("O que pretente fazer?\n1 - Adicionar auluno\n2- Ver Media\n3 - Deletar aluno\n4 - Sair "))
+    if h == 1:
+        y = int(input("Qual sala?\n1 - Sala 1\n2 - Sala 2 "))
+        if y == 1:
+            nome = input("Nome do aluno: ")
+            nota1 = int(input("Nota 1 do aluno: "))
+            nota2 = int(input("Nota 2 do aluno: "))
+            nota3 = int(input("Nota 3 do aluno: "))
+            aluno = Sala1(nome, nota1, nota2, nota3)
+            aluno.adicionar_aluno()
 
-  def adicionar_aluno(self):
-    sql = "INSERT INTO sala1 (nome, nota1, nota2, nota3, media) VALUES (%s, %s, %s, %s, %s)"
-    mediaCalc = (self.nota1 + self.nota2 + self.nota3) / 3
-    val = (self.nome, self.nota1, self.nota2, self.nota3, mediaCalc)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print('Aluno adicionado')
+        elif y == 2:
+            nome = input("Nome do aluno: ")
+            nota1 = int(input("Nota 1 do aluno: "))
+            nota2 = int(input("Nota 2 do aluno: "))
+            nota3 = int(input("Nota 3 do aluno: "))
+            aluno = Sala2(nome, nota1, nota2, nota3)
+            aluno.adicionar_aluno()
+    elif h == 2:
+        y = int(input("Qual sala?\n1 - Sala 1\n2 - Sala 2 "))
+        if y == 1:
+            x = int(input("Digite o ID: "))
+            media = Sala1.calcular_media(x)
+            print(media)
 
-  @staticmethod
-  def calcular_media(ID):
-    sql = (f"SELECT media FROM sala1 WHERE ID = '{ID}'")
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    return myresult
-
-class Sala2:
-  def __init__(self, nome, nota1, nota2, nota3):
-    self.nome = nome
-    self.nota1 = nota1
-    self.nota2 = nota2
-    self.nota3 = nota3
-
-  def adicionar_aluno(self):
-    sql = (f"INSERT INTO sala2 (nome, nota1, nota2, nota3, media) VALUES (%s, %s, %s, %s, %s)")
-    mediaCalc = (self.nota1 + self.nota2 + self.nota3) / 3
-    val = (self.nome, self.nota1, self.nota2, self.nota3, mediaCalc)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print('Aluno adicionado')
-
-  @staticmethod
-  def calcular_media(ID):
-    sql = (f"SELECT media FROM sala2 WHERE ID = '{ID}'")
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    return myresult
+        if y == 2:
+            x = int(input("Digite o ID: "))
+            media = Sala2.calcular_media(x)
+            print(media)
+    elif h == 4:
+        continuar = False
+    elif h == 3:
+        y = int(input("Qual sala?\n1 - Sala 1\n2 - Sala 2 "))
+        if y == 1:
+            x = int(input("Digite o ID: "))
+            media = Sala1.deletar_aluno(x)
+            print(media)
+        if y == 2:
+            x = int(input("Digite o ID: "))
+            media = Sala2.deletar_aluno(x)
+            print(media)
